c3eba549a37cc241fde6fe2000a8f37971c7c8ee
max <maxzer7@gmail.com>
2019-05-02 01:51:53 +0300

    crash webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
     entry: {
         app: './src/index.js'
@@ -8,14 +10,44 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist'
     },
-    devServer: {
-        overlay: true
-    },
     module: {
         rules: [{
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
+        }, {
+            test: /\.css$/,
+             use: [
+                 MiniCssExtractPlugin.loader,
+                 "css-loader"
+             ]
+         }, {
+            test: /\.scss$/,
+             use: [
+                 'style-loader',
+                 MiniCssExtractPlugin.loader,
+                 {
+                     loader: 'css-loader',
+                     options:  {sourceMap: true}
+                     },
+                 {
+                     loader: 'sass-loader',
+                     options:  {sourceMap: true},
+                 },
+                 {
+                     loader: 'postcss-loader',
+                     options:  {sourceMap: true, config: {path: 'src/js/postcss.config.js'}},
+                 }
+             ]
         }]
-    }
+    },
+
+    devServer: {
+        overlay: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        })
+    ],
 }
