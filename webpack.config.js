4aab8c7fde9207b0cd09406b35f0b09f2f42371a
max <maxzer7@gmail.com>
2019-05-01 16:10:17 +0300

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-    entry: './src/idex.js',
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
