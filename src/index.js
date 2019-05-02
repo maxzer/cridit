d795bf3d132338a67f954220b395a3744a5a6d24
max <maxzer7@gmail.com>
2019-05-02 23:22:34 +0300

    learning successfully

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
-import './js/common'
-import './css/main.css'
+// JS - ./js/index.js
+import './js/'
+// SCSS
 import './scss/main.scss'
+// CSS (example)
+import './css/main.css'
 
+// Bootstrap (example)
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 // import 'vue'
 // import Vue from 'vue'
-window.Vue = require('vue')
 
+//* Without vuex and store
+// const app = new Vue({
+//   el: '#app'
+// })
