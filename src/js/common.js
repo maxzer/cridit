08ccab9bf0e048d580ede2b061ef3b5ab07de7dd
max <maxzer7@gmail.com>
2019-05-05 00:16:42 +0300

    work in validation functional

--- src/js/common.js
+++ src/js/common.js
@@ -2,3 +2,26 @@ $('.js-left-menu').click(function () {
     $('.js-left-menu').removeClass('active');
     $(this).addClass('active');
 });
+
+$('.js-send').click(function () {
+    $('.js-card-input').each(function () {
+        let number = $(this).val().length;
+        if (number < 4){
+            $(this).addClass('error')
+        } else {
+            $(this).removeClass('error')
+        }
+    });
+
+    $('.js-pass-code').each(function () {
+        let number = $(this).val().length;
+
+        if (number < 4){
+            $(this).addClass('error')
+        } else {
+            $(this).removeClass('error')
+        }
+
+    })
+
+});
