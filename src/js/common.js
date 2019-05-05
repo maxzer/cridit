4722dec050e01d98a0d5ecf78dd101db76042f1c
max <maxzer7@gmail.com>
2019-05-05 12:56:32 +0300

    фвв validation functional

--- src/js/common.js
+++ src/js/common.js
@@ -4,24 +4,42 @@ $('.js-left-menu').click(function () {
 });
 
 $('.js-send').click(function () {
+
     $('.js-card-input').each(function () {
         let number = $(this).val().length;
         if (number < 4){
-            $(this).addClass('error')
+            $(this).addClass('error');
+
         } else {
-            $(this).removeClass('error')
+            $(this).removeClass('error');
         }
     });
 
     $('.js-pass-code').each(function () {
         let number = $(this).val().length;
 
-        if (number < 4){
+        if (number < 3){
             $(this).addClass('error')
         } else {
             $(this).removeClass('error')
         }
 
-    })
+    });
 
+    let cardName = $('.js-name');
+    var regexp = /^[a-z\s]+$/i;
+       if(!regexp.test($(cardName).val()) || $(cardName).val().length < 4) {
+           $(cardName).addClass('error')
+       } else {
+          $(cardName).removeClass('error')
+       }
+
+       let number = $('.js-card-input').hasClass('error');
+       let code = $('.js-pass-code').hasClass('error');
+       let name = $(cardName).hasClass('error');
+       if (number || code || name) {
+           alert('нет')
+       } else {
+           alert('отправлено ')
+       }
 });
